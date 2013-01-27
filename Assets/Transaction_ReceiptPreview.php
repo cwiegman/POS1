b30d99affd0f71b11203a598f6f8726478174b6b
Chris Wiegman <cw@multimediabychris.com>
2013-01-26 19:56:02 -0500

    design assets

---
+++ Assets/Transaction_ReceiptPreview.php
@@ -0,0 +1,38 @@
+<div id="Transaction_ReceiptPreview">
+      <table width="700" border="0" cellspacing="0" cellpadding="6">
+        <tr>
+          <td width="20%" align="center" bgcolor="#CCCCCC">
+          	<p>Todays Date<br />
+            	YEAR</p>
+			<p>Receipt</p>
+          <p class="receiptnumber">#</p></td>
+          <td bgcolor="#FFFFFF"><table border="0" width="100%">
+            <tr>
+              <td width="12%" align="right"><div id="noOfShooters_reciept"> </div></td>
+              <th width="71%" align="left"> Number of shooters <span id="amtOfTime_reciept"> </span></th>
+              <th width="17%" align="right"><div id="shoot_cost_reciept"> </div></th>
+            </tr>
+            <tr>
+              <td align="right"><div id="ProtectionRental_receipt"> </div></td>
+              <th align="left"> Eyes and Ears</th>
+              <th align="right"><div id="ProtectionRental_cost_receipt"> </div></th>
+            </tr>
+            <tr>
+              <td align="right"><div id="AmmoQty_receipt"> </div></td>
+              <th align="left"> Boxes of Ammo, <span id="AmmoType_receipt"> </span></th>
+              <th align="right"><div id="Ammo_cost_receipt"> </div></th>
+            </tr>
+              <tr>
+              <td align="right"><div id="Ammo2Qty_receipt"> </div></td>
+              <th align="left"> Boxes of Ammo #2, <span id="Ammo2Type_receipt"> </span></th>
+              <th align="right"><div id="Ammo2_cost_receipt"> </div></th>
+            </tr>
+            <tr>
+              <td align="right"><div id="TargetsQty_receipt"> </div></td>
+              <th align="left"> Targets, <span id="TargetType_receipt"> </span></th>
+              <th align="right"><div id="Target_cost_receipt"> </div></th>
+            </tr>
+          </table></td>
+          <td width="20%" bgcolor="#CC3300"><div id="Grand_Total_cost_receipt"> </div></td>
+        </tr>
+      </table> </div>
---
+++ Assets/navigation.php
@@ -0,0 +1,9 @@
+<div id="navigation">
+    <div class="Salesmenu"><a href="#" id="cancel">Cancel</a><a href="#" id="print">Print</a></div>
+    <div class="primarymenu">
+ <a href="#" id="SALE">SALE</a>
+ <a href="#" id="inventory">inventory</a>
+ <a href="#" id="REPORT">REPORT</a>
+ <a href="#" id="LOGOUT">LOGOUT</a>
+    </div>
+</div>
